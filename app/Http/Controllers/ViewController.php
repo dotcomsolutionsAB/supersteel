ad49c43f20e6b9012aa3f8c7e4d6f1d11125b372
Burhanuddin Hatim Kanchwala <dotcomsolutiononline@gmail.com>
2024-10-25 10:52:44 +0530

    ..

--- app/Http/Controllers/ViewController.php
+++ app/Http/Controllers/ViewController.php
@@ -141,9 +141,11 @@ class ViewController extends Controller
                 if ($cart_item) {
                     $product->in_cart = true;
                     $product->cart_quantity = $cart_item->quantity;
+                    $product->cart_quantity = $cart_item->remarks;
                 } else {
                     // If the product is not in the cart
                     $product->in_cart = false;
+                    $product->cart_quantity = null;  // or 0, depending on your preference
                     $product->cart_quantity = null;  // or 0, depending on your preference
                 }
             }
