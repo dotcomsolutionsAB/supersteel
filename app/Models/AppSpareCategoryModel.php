f92ba3a76cef6647eb34d50319f1917ef00ba5ef
Burhanuddin Hatim Kanchwala <dotcomsolutiononline@gmail.com>
2024-11-06 09:49:36 +0530

    ..

--- app/Models/AppSpareCategoryModel.php
+++ app/Models/AppSpareCategoryModel.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class AppSubCategoryModel extends Model
+class AppSpareCategoryModel extends Model
 {
     use HasFactory;
 
