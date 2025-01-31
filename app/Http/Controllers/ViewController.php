c99060b91b43fd9423356a4c3167e30152b44839
Burhanuddin Hatim Kanchwala <dotcomsolutiononline@gmail.com>
2025-01-31 17:48:37 +0530

    ..

--- app/Http/Controllers/ViewController.php
+++ app/Http/Controllers/ViewController.php
@@ -109,7 +109,7 @@ class ViewController extends Controller
 
         // If a valid price type is found, select that column as 'price'
         if (!empty($price_column)) {
-            $query->select('id', 'product_code', 'product_name', 'print_name', 'brand', 'category', 'type', 'machine_part_no', DB::raw("$price_column as price"),'ppc', 'product_image', 'new_arrival','special_price','video_link','extra_images', 'stock', 'pending', 'in_transit');
+            $query->select('id', 'product_code', 'product_name', 'print_name', 'brand', 'category', 'type', 'machine_part_no', DB::raw("$price_column as price"),'ppc', 'product_image', 'new_arrival','special_price','video_link','extra_images', 'stock', 'pending', 'in_transit','supplier');
         }
 
 
@@ -253,6 +253,7 @@ class ViewController extends Controller
             'category',
             DB::raw("$price_column as price"),
             'product_image',
+            'supplier',
             'ppc',
             'stock',
             'in_transit',
