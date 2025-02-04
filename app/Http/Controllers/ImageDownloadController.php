26c72f2830f2c24266f566535f4f55c77f557825
Burhanuddin Hatim Kanchwala <dotcomsolutiononline@gmail.com>
2025-02-04 20:42:15 +0530

    ..

--- app/Http/Controllers/ImageDownloadController.php
+++ app/Http/Controllers/ImageDownloadController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Storage;
-use Intervention\Image\Facades\Image;
+use Intervention\Image\ImageManager;
 use Carbon\Carbon;
 
 class ImageDownloadController extends Controller
