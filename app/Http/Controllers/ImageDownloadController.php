91a43360d22b305bd5860b99f95908c1e0c2a562
Burhanuddin Hatim Kanchwala <dotcomsolutiononline@gmail.com>
2025-02-05 21:20:04 +0530

    ..

--- app/Http/Controllers/ImageDownloadController.php
+++ app/Http/Controllers/ImageDownloadController.php
@@ -26,6 +26,7 @@ class ImageDownloadController extends Controller
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_TIMEOUT, 1000); // 120 seconds timeout
         curl_setopt($ch, CURLOPT_FAILONERROR, true);
+        curl_setopt($ch, CURLOPT_POST, true);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 
         $response = curl_exec($ch);
