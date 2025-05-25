114a01543ce54cbb487951cc79e34861bc50583a
Burhanuddin Hatim Kanchwala <dotcomsolutiononline@gmail.com>
2025-05-25 10:45:13 +0530

    ..

--- app/Http/Controllers/ViewController.php
+++ app/Http/Controllers/ViewController.php
@@ -261,7 +261,7 @@ class ViewController extends Controller
 
         // If a valid price type is found, select that column as 'price'
         if (!empty($price_column)) {
-            $query->select('id', 'product_code', 'print_name', 'brand', 'type', 'machine_part_no', DB::raw("$price_column as price"),'product_image', 'video_link','extra_images');
+            $query->select('id', 'product_code', 'print_name', 'brand', 'type', 'machine_part_no', DB::raw("$price_column as price"), 'video_link');
         }
 
         // Order by `sn` column
